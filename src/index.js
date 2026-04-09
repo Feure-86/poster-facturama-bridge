c437b76ccba64a57a05d8cdb771a1c7a32ddd0e6
Feure-86 <jesus.alvarez.manzanilla@opti-think.com>
2026-04-09 21:10:09 +0200

    Allow static portal CORS access

--- src/index.js
+++ src/index.js
@@ -15,6 +15,19 @@ const {
 const { buildMonthlyFinancialReports, DEFAULT_CHART_OF_ACCOUNTS } = require("./reporting/monthlyReports");
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, x-poster-webhook-secret, x-webhook-secret");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  return next();
+});
+
 app.use(express.json({ limit: "2mb" }));
 
 const posterTokenStore = new JsonStore(path.join(config.dataDir, "poster-tokens.json"));
